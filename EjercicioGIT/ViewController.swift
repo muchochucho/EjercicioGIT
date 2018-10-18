e7c7161aca4ea480282ac8b36d945b192fe6d5b1
nathanw <nat@mac11.local>
2018-10-18 18:36:04 -0500

    Initial Commit

---
+++ EjercicioGIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjercicioGIT
+//
+//  Created by nathanw on 18/10/18.
+//  Copyright © 2018 nathanw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
